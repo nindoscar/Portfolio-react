27d558876874c601ddfafc10e4a14609abadb0ea
nindoscar <oscar.nindebahi@epitech.eu>
2025-11-09 00:05:26 +0000

    [ADD] Complete the navigation bar

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 // import { nav } from "framer-motion/client";
-import React from "react";
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="fixed w-full z-50 bg-dark-100/99 backdrop-blur-sm py-4 px-8 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
@@ -18,45 +21,115 @@ const Navbar = () => {
             className="relative text-white/80 transition duration-300 hover:text-purple group"
           >
             <span>Home</span>
-            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 groupe-hover:w-full"></span>
+            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
           </a>
           <a
             href="#about"
             className="relative text-white/80 transition duration-300 hover:text-purple group"
           >
             <span>About</span>
-            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 groupe-hover:w-full"></span>
+            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
           </a>
           <a
             href="#Skills"
             className="relative text-white/80 transition duration-300 hover:text-purple group"
           >
             <span>Skills</span>
-            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 groupe-hover:w-full"></span>
+            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
           </a>
           <a
             href="#projects"
             className="relative text-white/80 transition duration-300 hover:text-purple group"
           >
             <span>Projects</span>
-            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 groupe-hover:w-full"></span>
+            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
           </a>
           <a
             href="#experience"
             className="relative text-white/80 transition duration-300 hover:text-purple group"
           >
             <span>Experiences</span>
-            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 groupe-hover:w-full"></span>
+            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
           </a>
           <a
             href="#contact"
             className="relative text-white/80 transition duration-300 hover:text-purple group"
           >
             <span>Contact</span>
-            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 groupe-hover:w-full"></span>
+            <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
           </a>
         </div>
+
+        {/* Burger Mobile */}
+
+        <div className="md:hidden">
+          {showMenu ? (
+            <FaXmark
+              onClick={() => setShowMenu(!showMenu)}
+              className="text-2xl cursor-pointer"
+            />
+          ) : (
+            <FaBars
+              onClick={() => setShowMenu(!showMenu)}
+              className="text-2xl cursor-pointer"
+            />
+          )}
+        </div>
+
+        {/* <div className="md:hidden">
+          {
+            showMenu?
+            <FaBars onClick={()=>setShowMenu(!showMenu)}
+            className="text-2xl cursor-pointer"/>:
+          <FaXmark onClick={()=>setShowMenu(!showMenu)}
+          className="text-2xl cursor-pointer"/>
+          }
+        </div>  */}
       </div>
+
+      {/* Menu Mobile */}
+
+      {showMenu && (
+        <div className="md:hidden mt-4 bg-dark-300 h-screen rounded-lg p-4 flex flex-col space-y-4 text-center justify-center">
+          <a onClick={() => setShowMenu(!showMenu)}
+            href="#home"
+            className="relative text-white/80 transition duration-300 hover:text-purple group"
+          >
+            <span>Home</span>
+            {/* <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span> */}
+          </a>
+          <a  onClick={() => setShowMenu(!showMenu)}
+            href="#about"
+            className="relative text-white/80 transition duration-300 hover:text-purple group"
+          >
+            <span>About</span>
+          </a>
+          <a  onClick={() => setShowMenu(!showMenu)}
+            href="#Skills"
+            className="relative text-white/80 transition duration-300 hover:text-purple group"
+          >
+            <span>Skills</span>
+          </a>
+          <a  onClick={() => setShowMenu(!showMenu)}
+            href="#projects"
+            className="relative text-white/80 transition duration-300 hover:text-purple group"
+          >
+            <span>Projects</span>
+          </a>
+          <a  onClick={() => setShowMenu(!showMenu)}
+            href="#experience"
+            className="relative text-white/80 transition duration-300 hover:text-purple group"
+          >
+            <span>Experiences</span>
+          </a>
+          <a  onClick={() => setShowMenu(!showMenu)}
+            href="#contact"
+            className="relative text-white/80 transition duration-300 hover:text-purple group"
+          >
+            <span>Contact</span>
+          </a>
+        </div>
+      )}
     </nav>
   );
 };
